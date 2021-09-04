f6795c33b3f955dfdb37bfce5fc2366fee4c4e89
ricardolemaciel <52418464+ricardolemaciel@users.noreply.github.com>
2021-09-04 17:21:16 -0300

    site

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+function abreMenu() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function fechaMenu() {
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
